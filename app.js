28b57c9684455a4d4523459299f26cf3175647fb
George Krivosheev <adept_kg@phystech.edu>
2017-11-14 12:51:25 +0300

    Task 3 Fix

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ function addElement(s) {
 }
 
 $(function(){
+    i = 1;
     $('#root').append('<ul></ul>');
     $('#root').append('<input id="add_task_input"> <button id="add_task">Добавить</button>');
-    $('#add_task').click(function(){addElement($('#add_task_input').val())});
-    addElement('Сделать задание #3 по web-программированию');
+    $('#add_task').click(function(){if (i == 1){addElement('Сделать задание #3 по web-программированию');} else {addElement($('#add_task_input').val())} ++i;});
+    
 })
